f8c48d80ace6a122ba682134e69040eafb7f9778
Turqay Tahirov <104727721+Turqay-Tahirov@users.noreply.github.com>
2026-07-20 11:40:18 +0400

    Update query2_left-join.sql

--- Week_1_SQL/Checkpoint_2/Week_1_SQL/Checkpoint_2/query2_left-join.sql
+++ Week_1_SQL/Checkpoint_2/Week_1_SQL/Checkpoint_2/query2_left-join.sql
@@ -1,1 +1,11 @@
+/*2 left join
+2. Bütün müştərilərin və əgər varsa, onların sifarişlərinin siyahısı hansıdır?
+İzah: left join vasitəsilə bütün müştərilər ekrana gətirilir və hələ heç bir sifariş
+verməyən müştərilər də siyahıda qorunur.*/
 
+select cus.CompanyName as "Müştəri", 
+  ord.OrderID as "Sifariş ID",
+  (emp.FirstName || ' ' || emp.LastName) as "Məsul İşçi" 
+  from Customers cus 
+  left join Orders ord on cus.CustomerID = ord.CustomerID 
+  left join Employees emp on ord.EmployeeID = emp.EmployeeID; 
