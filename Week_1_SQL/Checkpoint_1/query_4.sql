bf07b0f0f6e8291ead9afc9012b47b94354c0850
Turqay Tahirov <104727721+Turqay-Tahirov@users.noreply.github.com>
2026-07-23 19:32:06 +0400

    Delete Week_1_SQL/Checkpoint_1/query_4.sql

--- Week_1_SQL/Checkpoint_1/query_4.sql
+++
@@ -1,7 +0,0 @@
-/*Müştəriyə hələ göndərilməmiş ən təcili ilk 5 sifariş hansıdı? */
-/*Bu sorğu, yola çıxmayan sifarişləri çatdırılma vaxtına görə ən təcilidən başlayaraq sıralayır. */
-
-select OrderID, CustomerID, OrderDate, RequiredDate, ShippedDate from Orders 
-where ShippedDate is null
-order by RequiredDate asc
-limit 5;
